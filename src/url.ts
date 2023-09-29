c070ded9ae1114e6debac5b8c50160d026f127d5
Sven Viig <sven@dintero.com>
2023-09-29 10:54:52 +0200

    Use URLSearchParams in getSessionUrl also

--- src/url.ts
+++ src/url.ts
@@ -25,22 +25,29 @@ export const getSessionUrl = (options: SessionUrlOptions): string => {
     if (!endpoint) {
         throw new Error("Invalid endpoint");
     }
-
-    // Compose url for view session endpoint with optional language parameter.
-    const languageParam = language ? `language=${language}` : "";
-    const uiParam = ui ? `ui=${ui}` : "";
-    const sdk = `sdk=${pkg.version}`;
-    const validate = shouldCallValidateSession ? `client_side_validation=true` : undefined;
-    const role = popOut ? 'role=pop_out_launcher' : undefined;
-    const params = [languageParam, uiParam, sdk, validate, role].filter(x => x).join("&");
+    const params = new URLSearchParams();
+    params.append('sdk', pkg.version);
+    if(ui){
+        params.append('ui', ui);
+    }
+    if(language){
+        params.append('language', language);
+    }
+    if(shouldCallValidateSession){
+        params.append('client_side_validation', 'true');
+    }
+    if(popOut) {
+        params.append('role', 'pop_out_launcher');
+    }
     if (endpoint === "https://checkout.dintero.com") {
         // Default endpoint will redirect via the view endpoint
-        return `${endpoint}/v1/view/${sid}${params ? "?" + params : ""}`;
+        return `${endpoint}/v1/view/${sid}?${params.toString()}`;
     }
     // When a custom endpoint is set skip the view redirect endpoint since
     // custom endpoints like localhost and PR builds does not support the
     // serverside view flow.
-    return `${endpoint}/?sid=${sid}${params ? "&" + params : ""}`;
+    params.append('sid', sid);
+    return `${endpoint}/?${params.toString()}`;
 };
 
 const padTralingSlash = (endpoint: string) => endpoint.endsWith('/') ? endpoint : `${endpoint}/`;
