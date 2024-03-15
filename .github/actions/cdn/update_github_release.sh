caee53e88438655fd586648faad7fb15e6b08b7e
Anders Emil Salvesen <andersems@gmail.com>
2024-03-15 22:21:03 +0100

    Fixie

--- .github/actions/cdn/update_github_release.sh
+++ .github/actions/cdn/update_github_release.sh
@@ -6,19 +6,19 @@ set -euo pipefail
 URL=$1
 HASH=$2
 
-MULTI_LINES_TEXT="## Use from CDN: \n
-                  \`\`\`html \n
-                  <script \n
-                    src=\"${URL}\" \n
-                    integrity=\"sha-384-${HASH}\" \n
-                    crossorigin=\"anonymous\"> \n
-                  </script> \n
-                  \`\`\` \n
-                  "
+MULTI_LINES_TEXT="\n\n## Use from CDN: \n
+\`\`\`html
+<script
+  src=\"${URL}\"
+  integrity=\"sha-384-${HASH}\"
+  crossorigin=\"anonymous\">
+</script>
+\`\`\`
+"
 echo "$MULTI_LINES_TEXT"
 
 latest_tag=$(gh release list --json 'isLatest,tagName' --jq '.[] | select(.isLatest == true) | .tagName')
 current_body=$(gh release view "$latest_tag" --json body --jq '.body')
-amended_body="$current_body<br /><br />$MULTI_LINES_TEXT"
+amended_body="$current_body$MULTI_LINES_TEXT"
 
 echo -e "$amended_body" | gh release edit "$latest_tag" --notes-file -
