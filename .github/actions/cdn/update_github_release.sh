fa3e8e555680188a45f5c9ac8296aac6f0a10a83
Anders Emil Salvesen <anders@dintero.com>
2024-03-16 10:54:22 +0100

    fix(build): improve cdn usage markdown (#346)
    Improve the markdown used to update the github release.
    
    It will now look like this:
    
    > 
    > ### Use from CDN
    > 
    > ```html
    > <script
    >   src='https://assets.dintero.com/pkg/checkout-web-sdk/0.8.4/dintero-checkout-web-sdk.umd.min.js'
    >   integrity="sha-384-IkMn7gorZsjulSfpLhjhhvLd9M1pFJ08zxB2UPIV61+LJps2mMytucLzRDmsrYgV"
    >   crossorigin="anonymous">
    > </script>
    > ```

--- .github/actions/cdn/update_github_release.sh
+++ .github/actions/cdn/update_github_release.sh
@@ -3,8 +3,22 @@
 #  update_release.sh "text to append"
 set -euo pipefail
 
+URL=$1
+HASH=$2
+
+USE_FROM_CDN_EXAMPLE="\n\n### Use from CDN \n
+\`\`\`html
+<script
+  src=\"${URL}\"
+  integrity=\"sha-384-${HASH}\"
+  crossorigin=\"anonymous\">
+</script>
+\`\`\`
+"
+echo "$USE_FROM_CDN_EXAMPLE"
+
 latest_tag=$(gh release list --json 'isLatest,tagName' --jq '.[] | select(.isLatest == true) | .tagName')
 current_body=$(gh release view "$latest_tag" --json body --jq '.body')
-amended_body="$current_body<br /><br />$1"
+amended_body="$current_body$USE_FROM_CDN_EXAMPLE"
 
 echo -e "$amended_body" | gh release edit "$latest_tag" --notes-file -
